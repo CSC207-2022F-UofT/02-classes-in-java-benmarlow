a2eb4cccb09a20f404251bcd73ecc185d8edb6ea
benmarlow <113149564+benmarlow@users.noreply.github.com>
2022-09-11 18:30:45 -0400

    Update Silly.java

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -171,9 +171,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
-            private String s1 = this.name;
-            private String s2 = o.name;
-            return s1.equals(s2);
+            return this.name.equals(o.name);
         }
     }
 
