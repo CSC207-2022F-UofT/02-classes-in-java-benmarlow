7222e1529b39f9d49cad67eef2ab3bdd42253721
benmarlow <113149564+benmarlow@users.noreply.github.com>
2022-09-11 18:12:40 -0400

    Update Silly.java

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -166,7 +166,7 @@ public class Silly implements Comparable<Silly>{
         if (!(o instanceof Silly)){
             return false;
         } else {
-            return this.name.equals(o.name);
+            return this.name.equals(String(o));
         }
             
 
